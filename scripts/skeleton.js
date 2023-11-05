e10ddea246cc8b2eb7cc8dd9ae63aa3c94715fc9
cc <jonathanielalipes905@gmail.com>
2023-11-04 23:06:52 -0700

    renamed main.css -> welcome.css

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -7,13 +7,13 @@ function loadSkeleton() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
             // User is signed in.
-            // Do something for the user here.
+            // Do something for the user here. //after login state
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html')); //change this for sprint two
         } else {
-            // No user is signed in.
+            // No user is signed in. //before login state
             console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html')); // change this for sprint two
         }
     });
 }
