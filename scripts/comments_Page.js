5cd4997cb41679d4279764e7b279b340bf992dbb
maariaab <maferbaltaza@gmail.com>
2023-11-07 16:51:36 -0800

    added comment page and comment collection to firebase

--- scripts/comments_Page.js
+++ scripts/comments_Page.js
@@ -1,45 +1,40 @@
+
+
 //Initialize Firebase and get reference to the Firestore database
 const db2 = firebase.firestore();
 const auth2 = firebase.auth();
 const storage = firebase.storage();
 
 //Event listener for when activity ist posted:
-const activityForm = document.getElementById("postActivityForm");
+const activityForm = document.getElementById("comments");
 
 activityForm.addEventListener("submit", function(e) {
     e.preventDefault(); //prevent default form submission
 
     if(auth2.currentUser) {
         //Get Form Data:
-        const title = document.getElementById("Activity_Title").value;
-        const description = document.getElementById("Activity_Description").value;
-        const category = document.getElementById("CategorySelect").value;
-        const datetime = document.getElementById("DateTime").value;
-        const location = document.getElementById("Location").value;
-        const imageFile = document.getElementById("ActivityImage").files[0];
-
+        const userName = document.getElementById("name").value;
+        const description = document.getElementById("comment").value;
+       
         //Get the user's UID
         
         const userUID = auth2.currentUser.uid;
 
         //Reference to the activities collection:
-        const activitiesCollectionRef = db2.collection("Activities");
+        const activitiesCollectionRef = db2.collection("comments");
 
         //Data to store in the activity document:
         const activityData = {
-            title: title,
+            userName: userName,
             description: description,
-            category: category,
-            datetime: datetime,
-            location: location,
             userID: userUID
         };
 
 
-        //Add new activities document to the "Activities" collection:
+        //Add new activities document to the "Coments" collection:
         activitiesCollectionRef.add(activityData)
         .then(() => {
-            window.location.href = "thanks.html";
+            //window.location.href = "thanks.html";
             //console.log("Activity added with ID: ", activityDocRef.id);
 
             //Upload image to the Firebase Storage
