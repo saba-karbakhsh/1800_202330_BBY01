e3957323ccf56893a43515d88516297fdb924496
Liam Janicke <liam.jaenicke@gmail.com>
2023-11-11 10:39:16 -0800

    added join_activity.js which adds the user's name to a participants subcollection when joining an activity

--- scripts/join_activity.js
+++ scripts/join_activity.js
@@ -1,2 +1,58 @@
-//Puts the current user ID into a subcollection "participants"
-//for the corresponding activity that was joined.
+// Function to get the activity ID from the current URL
+const urlParams = new URLSearchParams(window.location.search);
+const currentActivityId = urlParams.get('docID');
+console.log("Activity ID: ", currentActivityId);
+
+// Now we can use currentActivityId in the joinActivity function
+const auth3 = firebase.auth();
+const firestore3 = firebase.firestore();
+
+// Function to handle joining an activity
+function joinActivity() {
+  
+  // Get the current user
+  const currentUser = auth3.currentUser;
+
+  // Check if a user is authenticated
+  if (currentUser) {
+    // Reference to the activity document
+    const activityRef = firestore3.collection("Activities").doc(currentActivityId);
+
+    // Reference to the participants subcollection
+    const participantsRef = activityRef.collection("participants");
+
+    const currentUserUID = currentUser.uid;
+    const userNameRef = firestore3.collection("Users").doc(currentUserUID);
+
+    // Declare userName outside the block
+    let userName;
+
+    // Retrieve the user's name
+    userNameRef.get().then((doc) => {
+      if (doc.exists) {
+        // Access the user's name from the document data
+        userName = doc.data().name;
+        console.log("User's Name:", userName);
+
+        // Add the current user to the participants subcollection
+        participantsRef.doc(currentUser.uid).set({
+          name: userName
+        }).then(() => {
+          console.log("User joined the activity!");
+        }).catch((error) => {
+          console.error("Error joining activity:", error);
+        });
+      } else {
+        console.log("No such document!");
+      }
+    }).catch((error) => {
+      console.error("Error getting user document:", error);
+    });
+
+  } else {
+    console.error("User not authenticated.");
+    
+  }
+
+}
+
