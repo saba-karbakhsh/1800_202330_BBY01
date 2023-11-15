d0b60a0b01c7ca9d18b4bc3823f156ed759112a1
Liam Janicke <liam.jaenicke@gmail.com>
2023-11-15 09:38:23 -0800

    profile pic can now be added and saved

--- scripts/firebaseAPI_BBY01.js
+++ scripts/firebaseAPI_BBY01.js
@@ -16,3 +16,4 @@ var firebaseConfig = {
 //--------------------------------------------
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+var storage = firebase.storage();
