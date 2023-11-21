5f4a722f3673d0adc2799337aba446d38c56577a
Saba karbakhsh <skarbakhshravari@my.bcit.ca>
2023-11-20 17:54:31 -0800

    fixed the chatting.

--- scripts/chat.js
+++ scripts/chat.js
@@ -12,53 +12,81 @@ activityForm.addEventListener("submit", function (e) {
         //Get Form Data:
         const message = document.getElementById("message").value;
         const receiverName = document.getElementById("receiver").value;
-        
         //Get the user's UID
-        const userUID = auth2.currentUser.uid;
-
+        userUID = auth2.currentUser.uid;
+        console.log(userUID);
         //Reference to the activities collection:
-        const activitiesCollectionRef = db.collection("Chats");
+        const chatsCollectionRef = db.collection("Chats");
 
-        //Data to store in the activity document:
-        const activityData = {
-            message: message,
-            receiverName: receiverName,
-            senderId: userUID
-        };
-
-
-        //Add new activities document to the "Activities" collection:
-        activitiesCollectionRef.add(activityData)
-            .then(() => {
-                activityForm.reset();
-
-                db.collection("Users").get().then(users => {
-                    users.forEach(userInfo => {
-                        receiverName1 = userInfo.data().name;
-                        if (receiverName == receiverName1) {
-                            // Retrieve the HTML element with the ID "chatCardTemplate" and store it in the cardTemplate variable.
-                            let cardTemplate = document.getElementById("chatCardTemplate");  
-                            // Clone the HTML template to create a new card (newCard) that will be filled with Firestore data.
-                            let newCard = cardTemplate.content.cloneNode(true); 
-                           
-                            //update title and location and description
-                            newCard.querySelector('.card-message').innerHTML = message;
-                          
-                             //attach to gallery"
-                            document.getElementById("chats-go-here").appendChild(newCard);
-
-                        }
-
-                    })
-                })
-            })
-
-            .catch((error) => {
-                console.error("Error adding chat: ", error);
+        db.collection("Users").get().then(users => {
+            users.forEach(userInfo => {
+                str = userInfo.data().name;
+                if (receiverName == str) {
+                    receiverId2 = userInfo.id;
+                    //Data to store in the activity document:
+                    const chatsData = {
+                        message: message,
+                        receiverName: receiverName,
+                        receiverId: receiverId2,
+                        senderId: userUID
+                    };
+                    //Add new activities document to the "Activities" collection:
+                    chatsCollectionRef.add(chatsData)
+                        .then(() => {
+                            activityForm.reset();
+            
+                        })
+            
+                        .catch((error) => {
+                            console.error("Error adding chat: ", error);
+                        });
+                }
             });
+        })
 
     } else {
         //If user isnt authenticated:
         console.log("User is not authenticated.");
     }
+
 })
+
+
+
+
+function displayChats(collection) {
+    let cardTemplate = document.getElementById("chatCardTemplate"); // Retrieve the HTML element with the ID "activityCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "Activities"
+        .then(allChats => {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allChats.forEach(doc => { //iterate thru each doc
+                var message = doc.data().message;       // get value of the "title" key
+                var receiverId = doc.data().receiverId;  // get value of the "description" key
+                var receiverName = doc.data().receiverName;    // get value of the "location" key
+                var senderId = doc.data().senderId; // get value of the "location" key
+                
+                let newCard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newCard) that will be filled with Firestore data.
+               
+                //update title and location and description
+                newCard.querySelector('.card-message').innerHTML = message;
+                
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery"
+                console.log(senderId);
+                if(auth2.currentUser.uid == senderId || auth2.currentUser.uid == receiverId){
+                    document.getElementById("chats-go-here").appendChild(newCard);
+
+                }
+                
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayChats("Chats");  //input param is the name of the collection
